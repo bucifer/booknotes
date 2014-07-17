f09fb748ec680ff09d552390311e9d5a3f4a0ea4
Terry Bu <ktbucifer@gmail.com>
2014-07-17 17:11:46 -0400

    got my image url scheme working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717014742) do
+ActiveRecord::Schema.define(version: 20140717151801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,14 +44,14 @@ ActiveRecord::Schema.define(version: 20140717014742) do
     t.integer  "book_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "username"
+  create_table "pictures", force: true do |t|
+    t.integer  "book_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
