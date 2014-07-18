26bed79e7aa541e9522a031af2db5cfb89184f79
Terry Bu <ktbucifer@gmail.com>
2014-07-18 15:27:56 -0400

    Got ckeditor working, made navbar styles change and got notes truncation strip tags working

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Booknotes
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     Paperclip.options[:command_path] = "/cygdrive/c/ImageMagick/convert"
+    config.assets.precompile += %w( ckeditor/ckeditor.js )
+
     end
 end
