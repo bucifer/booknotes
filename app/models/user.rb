48fcd15d4f7bb61a7befe58ce57e710d97f257ad
Terry Bu <ktbucifer@gmail.com>
2014-11-27 12:47:20 -0500

    Development mode works with S3 image uploading!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :password, length: { minimum: 3 }, presence: true
   validates :email, uniqueness: true, presence: true
-
+  
 end
